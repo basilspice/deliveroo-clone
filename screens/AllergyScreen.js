bec460b10eec71976f0c0a31a56d6d6a9d727a7b
basilspice <basil.breton@gmail.com>
2023-12-07 15:37:13 -0800

    created allergy screen w/assets

--- screens/AllergyScreen.js
+++ screens/AllergyScreen.js
@@ -1,16 +1,37 @@
-import { View, Text, SafeAreaView, Image } from "react-native";
-import { useNavigation, useRoute } from "@react-navigation/native";
-import { useLayoutEffect } from "react";
+import { View, Text, SafeAreaView, Image, TouchableOpacity } from "react-native";
+import React, { useEffect } from "react";
+import * as Animatable from "react-native-animatable";
+import {useNavigation} from "@react-navigation/native";
+import {XCircleIcon} from "react-native-heroicons/solid"
 
 const AllergyScreen = () => {
-  const navigation = useNavigation();
+
+const navigation = useNavigation();
+
 
   return (
-  <SafeAreaView>
-    <View>
-      <Image />
-    </View>
-  </SafeAreaView>
+    <SafeAreaView className="bg-[#00CCBB] flex-1 justify-center items-center ">
+      <TouchableOpacity
+            onPress={navigation.goBack}
+            className="rounded-full bg-gray-100 mb-10 "
+          >
+      <XCircleIcon size={40} color="black"/>
+     </TouchableOpacity>
+      <Animatable.Image
+        source={require("../assets/allergys.gif")}
+        animation="slideInUp"
+        iterationCount={1}
+        className="h-75 w-100"
+      />
+
+      <Animatable.Text
+      animation="slideInUp"
+      iterationCount={1}
+      className="text-lg my-10 text-white font-bold text-center"
+      >
+        Bummer
+      </Animatable.Text>
+    </SafeAreaView>
   );
 };
 
--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -62,7 +62,24 @@ const DeliveryScreen = () => {
           pinColor="#00CCBB"
         />
       </MapView>
+      <SafeAreaView className="bg-white flex-row items-center space-x-5 h-28">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-12 w-12 bg-gray-300 p-4 rounded-full ml-5"
+        />
+        <View className="flex-1">
+          <Text className="text-lg">Ricky</Text>
+          <Text className="text-gray-400">Your Rider</Text>
+        </View>
+<TouchableOpacity>
+
+        <Text className="text-[#00CCBB] text-lg mr-5 font-bold">Call</Text>
+</TouchableOpacity>
+      </SafeAreaView>
     </View>
+    
   );
 };
 
